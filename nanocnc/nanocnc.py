e95f54d4bf7ab537571ea27baaf25a91a00f9fd7
achkoe <achkoe@online.de>
2021-04-14 17:55:42 +0200

    updated

--- nanocnc/nanocnc.py
+++ nanocnc/nanocnc.py
@@ -10,10 +10,18 @@ Line -> Shapely
 """
 
 import sys
-from enum import Enum
+import enum
 from PyQt5 import QtWidgets, QtCore, QtGui
 
 import libnanocnc
+
+
+class Attribute(enum.Enum):
+    NONE = enum.auto()
+    INNER = enum.auto()
+    OUTER = enum.auto()
+    DISABLE = enum.auto()
+    IGNORE = enum.auto()
 
 
 class GraphicView(QtWidgets.QGraphicsView):
@@ -30,29 +38,45 @@ class GraphicView(QtWidgets.QGraphicsView):
         self.effect_b = QtWidgets.QGraphicsColorizeEffect()
         self.effect_b.setColor(self.color_b)
 
-    def drawPolygonList(self, polygonlist, clear=False):
+    def drawPolygonList(self, polygonlist, clear=False, pathattr=Attribute.NONE):
         if clear is True:
             self.setScene(QtWidgets.QGraphicsScene(QtCore.QRectF()))
         for polygon in polygonlist:
-            group = QtWidgets.QGraphicsItemGroup()
-            for index in range(len(polygon.xlist) - 1):
-                group.addToGroup(QtWidgets.QGraphicsLineItem(polygon.xlist[index], polygon.ylist[index],polygon.xlist[index + 1], polygon.ylist[index + 1]))
-            group._color = self.color_a
-            self.scene().addItem(group)
+            self.drawPolygon(polygon, pathattr)
         self.fitInView(self.scene().itemsBoundingRect(), QtCore.Qt.KeepAspectRatio)
         self.update()
+
+    def drawPolygon(self, polygon, pathattr):
+        group = QtWidgets.QGraphicsItemGroup()
+        #group = self.scene().createItemGroup([])
+        for index in range(len(polygon.xlist) - 1):
+            group.addToGroup(QtWidgets.QGraphicsLineItem(polygon.xlist[index], polygon.ylist[index],polygon.xlist[index + 1], polygon.ylist[index + 1]))
+        group._pathattr = pathattr
+        group._polygon = polygon
+        if pathattr == Attribute.IGNORE:
+            effect = QtWidgets.QGraphicsColorizeEffect()
+            effect.setColor(QtGui.QColor(0, 0, 255))
+            group.setGraphicsEffect(effect)
+        self.scene().addItem(group)
+        return group
+
+    def deleteGroup(self, group):
+        print("deleteGroup")
+        group.prepareGeometryChange()
+        self.scene().removeItem(group)
 
     def mousePressEvent(self, event):
         extension = 6
         pos = self.cursor().pos()
         scenePoint = self.mapToScene(self.mapFromGlobal(pos))
         rect = QtCore.QRectF(scenePoint.x() - extension, scenePoint.y() - extension, 2 * extension, 2 * extension)
-        itemlist = [item for item in self.scene().items(rect) if isinstance(item, QtWidgets.QGraphicsItemGroup)]
+        itemlist = [item for item in self.scene().items(rect) if isinstance(item, QtWidgets.QGraphicsItemGroup) and item._pathattr != Attribute.IGNORE]
         # itemlist = [item for item in self.scene().items(rect) if item in self.activegroup.childItems()]
-        print(itemlist)
+        #print(itemlist)
         if len(itemlist) != 1:
             return
         self.signal_groupselect.emit(itemlist[0])
+        return
 
         color = itemlist[0]._color
 
@@ -67,45 +91,59 @@ class GraphicView(QtWidgets.QGraphicsView):
         self.update()
 
 
-class MainWindow(QtWidgets.QMainWindow):
+class CommandWidget(QtWidgets.QWidget):
 
-    cutmode_enum = Enum("cutmode_enum", ["NONE", "INNER", "OUTER"])
+    signal_actionclicked = QtCore.pyqtSignal()
 
-    def __init__(self, filename=None):
+    def __init__(self, graphicview):
         super().__init__()
-        self.filename = filename
-        self.cutmode = self.cutmode_enum.NONE
-        self.graphicview = GraphicView()
-        mainwidget = QtWidgets.QWidget()
-        mainlayout = QtWidgets.QHBoxLayout()
-        mainlayout.addWidget(self.graphicview)
-        controllayout = QtWidgets.QVBoxLayout()
-        buttonlist = (
-            ("Zoom In", self.buttonZoomInClick), ("Zoom Out", self.buttonZoomOutClick),
-            ("Cut Outer", self.buttonCutOuterClick), ("Cut Inner", self.buttonCutInnerClick))
-        for (caption, method) in buttonlist:
-            button = QtWidgets.QPushButton(caption)
-            button.clicked.connect(method)
-            controllayout.addWidget(button)
-        mainlayout.addLayout(controllayout)
-        mainwidget.setLayout(mainlayout)
-        self.setCentralWidget(mainwidget)
-        if filename is not None:
-            self.loadSvgFile(filename)
-        self.graphicview.signal_groupselect.connect(self.groupSelect)
+        self.graphicview = graphicview
+        layout = QtWidgets.QVBoxLayout()
+        button = QtWidgets.QPushButton("Zoom In")
+        layout.addWidget(button)
+        button.clicked.connect(self.buttonZoomInClick)
+        button = QtWidgets.QPushButton("Zoom Out")
+        button.clicked.connect(self.buttonZoomOutClick)
+        layout.addWidget(button)
 
-    def groupSelect(self, item):
-        print(self.cutmode)
-        effect = QtWidgets.QGraphicsColorizeEffect()
-        if self.cutmode == self.cutmode_enum.INNER:
-            effect.setColor(QtGui.QColor(255, 0, 0))
-        elif self.cutmode == self.cutmode_enum.OUTER:
-            effect.setColor(QtGui.QColor(255, 255, 0))
-        else:
-            effect = None
-        item.setGraphicsEffect(effect)
-        self.graphicview.update()
-        self.cutmode = self.cutmode_enum.NONE
+        self.buttongroup = QtWidgets.QButtonGroup()
+        self.buttongroup.setExclusive(True)
+
+        button = QtWidgets.QPushButton("Cut Outer")
+        button._data = Attribute.OUTER
+        button.setCheckable(True)
+        button.clicked.connect(self.buttonActionClicked)
+        self.buttongroup.addButton(button)
+        layout.addWidget(button)
+
+        button = QtWidgets.QPushButton("Cut Inner")
+        button._data = Attribute.INNER
+        button.clicked.connect(self.buttonActionClicked)
+        self.buttongroup.addButton(button)
+        button.setCheckable(True)
+        layout.addWidget(button)
+
+        button = QtWidgets.QPushButton("No Cut")
+        button._data = Attribute.NONE
+        button.clicked.connect(self.buttonActionClicked)
+        self.buttongroup.addButton(button)
+        button.setCheckable(True)
+        layout.addWidget(button)
+
+        button = QtWidgets.QPushButton("Disable",)
+        button._data = Attribute.DISABLE
+        button.clicked.connect(self.buttonActionClicked)
+        self.buttongroup.addButton(button)
+        button.setCheckable(True)
+        layout.addWidget(button)
+
+        cbToolDiameter = QtWidgets.QComboBox()
+        cbToolDiameter.addItems(["1mm", "2mm", "3mm", "4mm"])
+        layout.addWidget(cbToolDiameter)
+        layout.addStretch(1)
+        self.setLayout(layout)
+
+        self.cutmode = Attribute.NONE
 
     def buttonZoomOutClick(self):
         self.graphicview.scale(1 / 1.2, 1 / 1.2)
@@ -113,11 +151,74 @@ class MainWindow(QtWidgets.QMainWindow):
     def buttonZoomInClick(self):
         self.graphicview.scale(1.2, 1.2)
 
-    def buttonCutInnerClick(self):
-        self.cutmode = self.cutmode_enum.INNER
+    def buttonActionClicked(self):
+        button = self.sender()
+        [other.setChecked(False) for other in self.buttongroup.buttons() if other != button]
+        print(button, button.isChecked())
+        self.cutmode = button._data
+        self.signal_actionclicked.emit()
 
-    def buttonCutOuterClick(self):
-        self.cutmode = self.cutmode_enum.OUTER
+
+class MainWindow(QtWidgets.QMainWindow):
+
+    def __init__(self, filename=None):
+        super().__init__()
+        self.filename = filename
+        self.cutmode = Attribute.NONE
+        self.graphicview = GraphicView()
+        self.graphicview.signal_groupselect.connect(self.groupSelect)
+
+        self.commandwidget = CommandWidget(self.graphicview)
+
+        dockWidget = QtWidgets.QDockWidget("Commands")
+        dockWidget.setFeatures(QtWidgets.QDockWidget.DockWidgetMovable)
+        dockWidget.setAllowedAreas(QtCore.Qt.LeftDockWidgetArea | QtCore.Qt.RightDockWidgetArea)
+        dockWidget.setWidget(self.commandwidget)
+        self.addDockWidget(QtCore.Qt.LeftDockWidgetArea, dockWidget)
+
+        mainlayout = QtWidgets.QHBoxLayout()
+        mainlayout.addWidget(self.graphicview)
+        self.setCentralWidget(self.graphicview)
+        if filename is not None:
+            self.loadSvgFile(filename)
+
+    def groupSelect(self, item):
+        if self.commandwidget.cutmode == Attribute.NONE:
+            item._pathattr = Attribute.NONE
+            effect = QtWidgets.QGraphicsColorizeEffect()
+            effect.setColor(QtGui.QColor(0, 0, 0))
+            item.setGraphicsEffect(effect)
+            group = getattr(item, "_group", None)
+            if group is not None:
+                self.graphicview.deleteGroup(group)
+                item._group = None
+            print("44")
+        elif self.commandwidget.cutmode == Attribute.INNER:
+            if item._pathattr == Attribute.NONE:
+                print("INNER")
+                group = self.graphicview.drawPolygon(item._polygon.expand(2), pathattr=Attribute.IGNORE)
+                item._group = group
+                item._pathattr = Attribute.INNER
+        elif self.commandwidget.cutmode == Attribute.OUTER:
+            if item._pathattr == Attribute.NONE:
+                print("OUTER")
+                group = self.graphicview.drawPolygon(item._polygon.expand(-2), pathattr=Attribute.IGNORE)
+                item._group = group
+                item._pathattr = Attribute.OUTER
+        elif self.commandwidget.cutmode == Attribute.DISABLE:
+            if item._pathattr == Attribute.NONE:
+                print("DISABLE")
+                effect = QtWidgets.QGraphicsColorizeEffect()
+                effect.setColor(QtGui.QColor(128, 128, 128))
+                item.setGraphicsEffect(effect)
+                item._pathattr = Attribute.DISABLE
+        elif self.commandwidget.cutmode == Attribute.IGNORE:
+            pass
+        else:
+            raise AttributeError(self.commandwidget.cutmode)
+
+        self.graphicview.update()
+        self.cutmode = Attribute.NONE
 
     def loadSvgFile(self, filename):
         polygonlist = libnanocnc.svg2polygon(filename)
